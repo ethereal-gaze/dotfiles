e6297b47e197fda8091127ed77b5636c34b868dc
Ben <75270146+ethereal-gaze@users.noreply.github.com>
2022-11-23 11:07:17 +0000

    updated

--- dwm/config.h
+++ dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        			/* border pixel of windows */
+static const unsigned int borderpx  = 1;        			/* border pixel of windows */
 static const unsigned int snap      = 32;       			/* snap pixel */
 static const unsigned int systraypinning = 0;   			/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   				/* 0: systray in the right corner, >0: systray on left of status text */
@@ -9,10 +9,10 @@ static const unsigned int systrayspacing = 2;   			/* systray spacing */
 static const int systraypinningfailfirst = 1;   			/* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     				/* 0 means no systray */
 static const int showbar            = 1;     				/* 0 means no bar */
-static const unsigned int gappx     = 8;        			/* gaps between windows */
+static const unsigned int gappx     = 0;        			/* gaps between windows */
 static const int topbar             = 1;     				/* 0 means bottom bar */
-static const char *fonts[]          = { "ttf-inconsolata:size=12" };
-static const char dmenufont[]       = "ttf-inconsolata:size=12";
+static const char *fonts[]          = { "undefined:medium:size=12" };
+static const char dmenufont[]       = "undefined:medium:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -20,16 +20,24 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 
 // My custom color variables
+static const char col_soviet_red[]  = "#cd0000";
+static const char col_dark_red[]    = "#8b0000";
+static const char col_soviet_gold[] = "#ffd900";
 static const char col_purple[]      = "#600C5C";
 static const char col_purple_dark[] = "#140625";
 static const char col_crimson[]     = "#842222";
 static const char col_sea_white[]   = "#FFF5EE";
+static const char col_cool_dark[]   = "#1d1f21";
+static const char col_light_grey[]  = "#cccccc";
+static const char col_text_grey[]   = "#bcc2cd";
+static const char col_sel_grey[]    = "#383a59";
+static const char col_bg_grey[]     = "#282a36";
 //
 
 static const char *colors[][3] = {
         /*             fg               bg                      border   */
-        [SchemeNorm] = { col_sea_white, col_purple_dark,        col_purple_dark },
-        [SchemeSel]  = { col_sea_white, col_purple,             col_purple  },
+        [SchemeNorm] = {col_text_grey, col_bg_grey,   col_bg_grey},
+        [SchemeSel]  = {col_text_grey, col_sel_grey,  col_sel_grey  },
 };
 
 /* tagging */
@@ -71,8 +79,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_purple_dark, "-nf", col_sea_white, "-sb", col_purple, "-sf", col_sea_white, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg_grey, "-nf", col_text_grey, "-sb", col_sel_grey, "-sf", col_text_grey, NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
